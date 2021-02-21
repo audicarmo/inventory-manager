d7b19ed71f32afc6e35744f1468a33b651ddccc0
Audinéia Carmo <audineia.carmo@ilia.digital>
2021-02-21 17:26:39 -0300

    :recycle: Add Src

---
+++ src/main/java/br/com/poupa/inventorymanager/InventoryManagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.poupa.inventorymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/poupa/inventorymanager/InventoryManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.poupa.inventorymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
